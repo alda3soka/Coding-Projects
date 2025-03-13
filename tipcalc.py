c802696c3232578a628afcaf658da7f015f222f3
alda3soka <hassan2006uni@gmail.com>
2025-03-13 10:33:48 +0300

    Delete tipcalc.py

--- tipcalc.py
+++
@@ -1,7 +0,0 @@
-import time
-total = float(input("Enter the amount you paid for food: "))
-tip = float(input("Enter the percentage you would like to tip: "))
-tipcalc = total * (tip / 100)
-print(f"Your total tip is ${tipcalc:02}")
-print(f"Your total amount paid is ${tipcalc + total:02}")
-time.sleep(3)
